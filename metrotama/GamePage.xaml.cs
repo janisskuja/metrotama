e8d9558b9e96fec16de159658f57eee66f6f7b3f
janis.skuja <janis.skuja@d8corp.com>
2013-04-24 19:24:42 +0300

    some changes

--- metrotama/GamePage.xaml.cs
+++ metrotama/GamePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Linq;
+using TCD.Controls;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using MonoGame.Framework;
 
@@ -8,7 +10,7 @@ namespace metrotama
     /// <summary>
     /// The root page used to display the game.
     /// </summary>
-    public sealed partial class GamePage : SwapChainBackgroundPanel
+    public sealed partial class GamePage : LayoutAwarePage
     {
         readonly Game1 _game;
 
@@ -17,7 +19,31 @@ namespace metrotama
             this.InitializeComponent();
 
             // Create the game.
-            _game = XamlGame<Game1>.Create(launchArguments, Window.Current.CoreWindow, this);
+            _game = XamlGame<Game1>.Create(launchArguments, Window.Current.CoreWindow, DxSwapChainPanel);
+        }
+
+        private void AppBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Get the app bar's root Panel.
+            var root = ((AppBar)sender).Content as Panel;
+            if (root == null) return;
+            // Get the Panels that hold the controls.
+            foreach (var child in root.Children.Cast<Panel>().SelectMany(panel => panel.Children))
+            {
+                base.StartLayoutUpdates(child, new RoutedEventArgs());
+            }
+        }
+
+        private void AppBar_Unloaded(object sender, RoutedEventArgs e)
+        {
+            // Get the app bar's root Panel.
+            var root = ((AppBar)sender).Content as Panel;
+            if (root == null) return;
+            // Get the Panels that hold the controls.
+            foreach (var child in root.Children.Cast<Panel>().SelectMany(panel => panel.Children))
+            {
+                base.StopLayoutUpdates(child, new RoutedEventArgs());
+            }
         }
     }
 }
